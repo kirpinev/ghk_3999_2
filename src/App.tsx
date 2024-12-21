b5a90d048fc8b473b5f967aa2eb11c8d293edc9d
Igor Kirpinev <kirpinevigor@yandex.ru>
2024-12-21 14:24:51 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -636,7 +636,7 @@ export const App = () => {
         </div>
       )}
 
-      <Gap size={96} />
+      {step === 1 && <Gap size={96} />}
 
       {step === 1 && (
         <div className={appSt.bottomBtnThx}>
